a91f1af406effbbd95333033f5862c8a15eeea20
dmKopylets <dm.kopylets@gmail.com>
2025-11-12 15:50:13 +0200

    small fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-//        $this->call(AdminSeeder::class);
-//        $this->call(UserSeeder::class);
+        $this->call(AdminSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(CustomerSeeder::class);
         $this->call(TicketSeeder::class);
     }
